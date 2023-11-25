6236840b75f619093f6124f108c899e6dde00414
24746 <2474674768@qq.com>
2023-11-25 20:10:27 +0800

    fix special symbol in title

--- code/inverted_index.py
+++ code/inverted_index.py
@@ -1,10 +1,11 @@
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch(['https://hh:123456@localhost:7000'], request_timeout=3600)
-# 创建一个索引为test的索引
+es = Elasticsearch(['http://localhost:9200'], request_timeout=3600)
+
 doc = {
     'name': '杨晨',
     'age': '22'
 }
+
 # 在test索引id为3的位置插入一条数据
-es.create(index='test',id='3', document=doc)
+es.create(index='test_0', id='3', body=doc)
