e2bdb63dd018ebf2fa66d2074f6f25fdb306ada1
24746 <2474674768@qq.com>
2023-12-02 14:53:21 +0800

    insert reviews into database

--- code/database.py
+++ code/database.py
@@ -6,7 +6,7 @@ db = pymysql.connect(
     port=3306,				# 端口号，默认为3306
     user='root',			# 用户名
     password='zhu203545',	# 密码
-    charset='utf8'  		# 设置字符编码
+    charset='utf8mb4'  		# 设置字符编码
 )
 # 创建游标对象
 cursor = db.cursor()
@@ -25,7 +25,7 @@ def create_movie_table():
             导演 TEXT,
             演员 TEXT,
             简介 TEXT
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
     """
     cursor.execute(sql)
 
@@ -34,16 +34,17 @@ def create_review_table():
     sql = """
         CREATE TABLE IF NOT EXISTS reviews (
             id INT(11) PRIMARY KEY AUTO_INCREMENT,
-            movie_id INT(11) NOT NULL FOREIGN KEY REFERENCES movies(id),
+            movie_id INT(11) ,
             电影名 VARCHAR(100) NOT NULL,
-            标题 VARCHAR(100) NOT NULL,
+            标题 VARCHAR(150) NOT NULL,
             作者 VARCHAR(30) NOT NULL,
             时间 DATETIME NOT NULL,
             作者评分 DECIMAL,
-            影评 TEXT NOT NULL,
+            影评 MEDIUMTEXT NOT NULL,
             有用数 INT(11),
-            没用数 INT(11)
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+            没用数 INT(11),
+            FOREIGN KEY(movie_id) REFERENCES movies(id)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
     """
     cursor.execute(sql)
 
@@ -80,34 +81,40 @@ def insert_movie(index):
     try:
         cursor.execute(sql)
         db.commit()
-    except:
+    except Exception as e:
         print("插入失败{}".format(index))
 
 def insert_review(index):
     # 解析XML文件
-    review = get_root(index)
+    root = get_root(index)
     # 电影名
-    movie_name = review.find('电影名').text.replace('"', '\"')
+    movie_name = root.find('电影名').text.replace('"', '\\"')
     # 遍历所有的review，解析xml
     for i, review in enumerate(root.iter('review')):
         # find all child elements
-        author = review.find('作者').text
+        author = review.find('作者').text.replace('"', '\\"')
         try:
-            title = review.find('标题').text
+            title = review.find('标题').text.replace('"', '\\"')
         except:
             title = ''
         try:
             author_rating = review.find('作者评分').text
+            if author_rating is None:
+                author_rating = '0'
         except:
-            author_rating = ''
+            author_rating = '0'
         time = review.find('时间').text
-        review_text = review.find('影评').text
+        review_text = review.find('影评').text.replace('"', '\\"')
         try:
             useful = review.find('有用数').text
+            if useful is None:
+                useful = '0'
         except:
             useful = '0'
         try:
             useless = review.find('没用数').text
+            if useless is None:
+                useless = '0'
         except:
             useless = '0'
 
@@ -129,45 +136,23 @@ def insert_review(index):
             INSERT INTO reviews (movie_id, 电影名, 标题, 作者, 时间, 作者评分, 影评, 有用数, 没用数)
             VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")
         """ %(data['movie_id'], data['电影名'], data['标题'], data['作者'], data['时间'], data['作者评分'], data['影评'], data['有用数'], data['没用数'])
-        #print(sql)
         try:
             cursor.execute(sql)
-        except:
-            print("插入失败{}th 电影，{}th评论".format(index, i))
+        except Exception as e:
+            print("插入失败，{}th 电影，{}th评论".format(index, i))
+            print(e)
+            print(sql)
+            exit(-1)
     db.commit()
 
 if __name__ == '__main__':
     # 创建数据库
-    #create_movie_table()
-    #create_review_table()
-    #for i in range(MOVIE_NUM):
+    create_movie_table()
+    create_review_table()
+
+    for i in range(MOVIE_NUM):
         #insert_movie(i)
-    #insert_movie(1241)
-    # Parse XML file
-    tree = ET.parse('../7.xml', ET.XMLParser(encoding='utf-8'))
-    root = tree.getroot()
-
-    # Find all 'review' elements
-    for review in root.iter('review'):
-        # For each 'review', find and print all child elements
-        author = review.find('作者').text
-        try:
-            title = review.find('标题').text
-        except:
-            title = ''
-        try:
-            author_rating = review.find('作者评分').text
-        except:
-            author_rating = ''
-        time = review.find('时间').text
-        review_text = review.find('影评').text
-
-        print("作者:", author)
-        print("标题: ", title)
-        print("作者评分: ", author_rating)
-        print("时间: ", time)
-        #print("影评: ", review_text)
-
+        insert_review(i)
 
 # 关闭游标和连接
 cursor.close()
