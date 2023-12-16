fb43d2252ea09d0315badbc7c5e3544fe622fef9
24746 <2474674768@qq.com>
2023-12-16 10:29:24 +0800

    search with pagerank

--- code/index.py
+++ code/index.py
@@ -6,15 +6,35 @@ DATA_PATH = '../data/'
 
 def create_index(name):
     # 创建索引
-    es.indices.create(index=name, ignore=400)
+    if not es.indices.exists(index=name):
+        es.indices.create(index=name)
 
 def search_index(name, str, num):
         # Define your search query
+    if name == 'reviews':
+        field = ['标题', '作者', '电影名', '影评']
+    else:
+        field = ['电影名', '导演', '演员', '简介']
     search_query = {
         "query": {
-            "multi_match": {
-                "query": str,
-                "fields": ["*"]
+            "function_score": {
+                "query": {
+                    "multi_match": {
+                        "query": str,
+                        "fields": ["*"]
+                    }
+                },
+                "functions": [
+                    {
+                        "field_value_factor": {
+                            "field": "pagerank",
+                            "factor": 1.0,
+                            "modifier": "log1p",
+                            "missing": 1
+                        }
+                    }
+                ],
+                "boost_mode": "multiply"
             }
         },
         "from": 0,
@@ -24,7 +44,7 @@ def search_index(name, str, num):
     response = es.search(index=name, body=search_query)
     # Print the titles of the returned documents
     for hit in response['hits']['hits']:
-        print(hit['_source']['电影名'])
+        print(hit['_source']['电影名'], hit['_source']['url'])
 
 if __name__ == '__main__':
     # 创建索引
@@ -36,4 +56,4 @@ if __name__ == '__main__':
         #insert_movie(i)
     
     # 搜索
-    search_index('movies', '勇气', 10)
+    search_index('movies', '海洋', 10)
